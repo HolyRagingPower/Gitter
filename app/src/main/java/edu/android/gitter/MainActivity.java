2cc0d0c7b8cf401b876c46c4619766b978da448b
HolyRagingPower <38124197+HolyRagingPower@users.noreply.github.com>
2018-04-13 13:43:57 +0900

    Update MainActivity.java

--- app/src/main/java/edu/android/gitter/MainActivity.java
+++ app/src/main/java/edu/android/gitter/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=findViewById(R.id.txt);
-        button=findViewById(R.id.txt);
+        button=findViewById(R.id.button);
     }
 }
 
